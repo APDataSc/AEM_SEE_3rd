151c71a8581b23c23b7ac1d34b8929348e4f4546
APDataSc <sklan8@hotmail.com>
2025-07-09 21:22:07 -0500

    Last script update!!!

--- script/S1_script.R
+++ script/S1_script.R
@@ -15,7 +15,7 @@ library(survey)
 library(calidad)
 library(rio)
 library(foreign)
-
+library(tictoc)
 
 # 3.1) Observar las funciones de un paquete
 ls("package:calidad")
@@ -71,11 +71,43 @@ prop.table(table(fac))
 
 
 # Manejo de data.frames en R
+tic()
 data <- read.spss("10 ENIGHUR11_HOGARES_AGREGADOS.SAV",
-                  to.data.frame = T, use.value.labels = F
+                  to.data.frame = T, use.value.labels = T
                   )
+toc()
+
+tic()
+data1 <- rio::import("10 ENIGHUR11_HOGARES_AGREGADOS.SAV")
+toc()
+
+class(data1)
 
 
+# Análisis de encuesta
+
+sum(data$Fexp_cen2010)
+mean(data$numpers)
+3923123*3.873186
+
+names(data)
+
+data <- data %>% select(Fexp_cen2010, Provincia, Área, sexo,
+                        edad, ing_cor_tot, gas_cor_tot, d1, numpers) 
+
+#Ctrl+Shift+m
+#Alt+"-"
+tab1 <- data %>%
+             group_by(Provincia) %>% 
+             summarise(media_ing = mean(ing_cor_tot)) %>% 
+             arrange(desc(media_ing))
+
+# Creación de variables  
+data <- data %>% 
+             mutate(ing_per = ing_cor_tot/numpers,
+                    pobre = ifelse(ing_per<80, 1, 0))
+  
+  
 
 
 
